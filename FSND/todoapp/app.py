19686b915504136340a60193a5c2938c6ea6d93c
Ali <ali.althuwaini@outlook.com>
2020-12-17 14:33:53 +0300

    Convert listed items from hard coded to database and let flask read the from there

--- FSND/todoapp/app.py
+++ FSND/todoapp/app.py
@@ -1,13 +1,27 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#connect to db: Note: you need to create the db 'todoapp' manually through psql before running this app (Just emplty db)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ali:123@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String(), nullable= False)
+    
+    # define dunder repr method
+    def __repr__(self):
+        return f'<Todo: {self.id}, desc: {self.description}>'
+
+db.create_all()
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', data = [
-        {'description': 'Todo 1'},
-        {'description': 'Todo 2'},
-        {'description': 'Todo 3'},
-        {'description': 'Todo 4'}
-    ])
+    return render_template('index.html', data = Todo.query.all())
