fb3d10f4d696b620a37c0737c65fb756e7d5cbfc
Ali <ali.althuwaini@outlook.com>
2020-12-18 13:40:48 +0300

    Create a todo item directly from the webpage

--- FSND/todoapp/app.py
+++ FSND/todoapp/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask.helpers import url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import redirect
 
 
 app = Flask(__name__)
@@ -20,6 +22,27 @@ class Todo(db.Model):
         return f'<Todo: {self.id}, desc: {self.description}>'
 
 db.create_all()
+
+# This route listen to request to create a new todo item and control that process
+@app.route('/todos/create', methods= ['POST'])
+def create_todo():
+    #This assumes that the post request will come as a form and hence, we can use request.form to handle it.
+
+    #This will get the the request input from field name=descrioption from the form in the index.html view and we default it to empty string in case there is not input.
+    description =  request.form.get('description', '')
+
+    #Now use the above description variable to create a new todo record in our database
+    #step1: use Todo class to create a column description from our variable description
+    todo = Todo(description= description)
+    #step2: add that column to our database - putting it in the pending stage
+    db.session.add(todo)
+    #step3: commit that change in order to take effect in the database
+    db.session.commit()
+    
+    #Now we want to go back to user, but what should we display? let's let the view to redirect to the index route and reshow the index page. index route will grap a fresh of all records in the todos table as a result of 
+    # data = Todo.query.all()
+    return redirect(url_for('index')) #index here is the function name of the handler used to listen to our index route 
+
 
 
 @app.route('/')
